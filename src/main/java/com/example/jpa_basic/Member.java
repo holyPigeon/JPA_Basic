a90a734c9603d6745554aff61608be5aa3881555
jae woo Jung <awfjol2008@naver.com>
2023-02-22 19:29:16 +0900

    Member 엔티티 필드 및 컬럼 매핑

--- src/main/java/com/example/jpa_basic/Member.java
+++ src/main/java/com/example/jpa_basic/Member.java
@@ -1,15 +1,32 @@
 package com.example.jpa_basic;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
-@Entity
+import java.time.LocalDateTime;
+
+@Entity // JPA를 사용해서 테이블과 매핑할 클래스
 @Getter @Setter
 public class Member {
 
   @Id
   private Long id;
-  private String name;
+
+  @Column(name = "name", nullable = false)
+  private String username;
+
+  @Enumerated(EnumType.STRING)
+  private RoleType rolltype;
+
+  private LocalDateTime createdDate;
+
+  private LocalDateTime lastModifiedDate;
+
+  @Lob
+  private String description;
+
+  public Member() {
+
+  }
 }

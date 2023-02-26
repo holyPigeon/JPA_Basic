2c2ffb952cec8748e40fffa1655e83d41c21844c
jae woo Jung <awfjol2008@naver.com>
2023-02-26 23:04:13 +0900

    Address 값 타입에 equals(), hasCode() 추가

--- src/main/java/com/example/jpa_basic/domain/member/Address.java
+++ src/main/java/com/example/jpa_basic/domain/member/Address.java
@@ -4,6 +4,8 @@ import jakarta.persistence.Embeddable;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Objects;
+
 @Embeddable
 @Getter
 public class Address {
@@ -13,4 +15,17 @@ public class Address {
   private String street;
 
   private String zipcode;
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    Address address = (Address) o;
+    return Objects.equals(getCity(), address.getCity()) && Objects.equals(getStreet(), address.getStreet()) && Objects.equals(getZipcode(), address.getZipcode());
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(getCity(), getStreet(), getZipcode());
+  }
 }

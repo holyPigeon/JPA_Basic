cc8f51066cbb945e3d600d99d1504b363f677f2c
jae woo Jung <awfjol2008@naver.com>
2023-02-26 16:18:36 +0900

    Category 엔티티 Cascade 설정

--- src/main/java/com/example/jpa_basic/domain/Category.java
+++ src/main/java/com/example/jpa_basic/domain/Category.java
@@ -27,6 +27,6 @@ public class Category extends BaseEntity {
   @JoinColumn(name = "PARENT_ID")
   private Category parent;
 
-  @OneToMany(mappedBy = "parent")
+  @OneToMany(mappedBy = "parent", cascade = CascadeType.ALL)
   private List<Category> child = new ArrayList<>();
 }
